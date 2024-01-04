9072476cab359c57cac32ff976c2e32c94089467
Saltator <saltatorv@gmail.com>
2024-01-04 17:42:08 +0100

    fix: fix typo in request mapping #3

--- video-management-application/src/main/java/com/video/management/application/api/UserCommandController.java
+++ video-management-application/src/main/java/com/video/management/application/api/UserCommandController.java
@@ -3,7 +3,7 @@ package com.video.management.application.api;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserCommandController {
     @PostMapping("{username}")
     public String addMovieToFavorites(@PathVariable String username, @RequestBody String title) {
