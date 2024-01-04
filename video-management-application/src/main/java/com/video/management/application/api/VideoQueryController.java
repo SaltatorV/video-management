b68d0c571ddbe428810c335bac21b70dc23b29d5
Saltator <saltatorv@gmail.com>
2024-01-04 18:23:00 +0100

    feat: update video query controller with facade interface #3

--- video-management-application/src/main/java/com/video/management/application/api/VideoQueryController.java
+++ video-management-application/src/main/java/com/video/management/application/api/VideoQueryController.java
@@ -1,5 +1,7 @@
 package com.video.management.application.api;
 
+import com.video.management.service.port.input.VideoQueryFacade;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/movies")
-public class MovieController {
+@RequiredArgsConstructor
+public class VideoQueryController {
 
+    private final VideoQueryFacade facade;
     @GetMapping("{title}")
-    public Object fetchMovie(@PathVariable String title) {
-        return title;
+    public Object fetchVideo(@PathVariable String title) {
+        return facade.findVideo(title);
     }
 }
