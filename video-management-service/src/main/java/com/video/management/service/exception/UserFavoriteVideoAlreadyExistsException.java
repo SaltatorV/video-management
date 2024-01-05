b17e109e6649a0d76da90e4df330fd3d779b70f2
Saltator <saltatorv@gmail.com>
2024-01-05 17:42:06 +0100

    refactor: rename base exception #6

--- video-management-service/src/main/java/com/video/management/service/exception/UserFavoriteVideoAlreadyExistsException.java
+++ video-management-service/src/main/java/com/video/management/service/exception/UserFavoriteVideoAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.video.management.service.exception;
 
-public class UserFavoriteVideoAlreadyExistsException extends VideoManagementDomainException{
+public class UserFavoriteVideoAlreadyExistsException extends DomainException {
     public UserFavoriteVideoAlreadyExistsException() {
         super("The user already has the specified film in their favorites.");
     }
--- video-management-service/src/main/java/com/video/management/service/exception/VideoManagementDomainException.java
+++
@@ -1,7 +0,0 @@
-package com.video.management.service.exception;
-
-public abstract class VideoManagementDomainException extends RuntimeException {
-    public VideoManagementDomainException(String message) {
-        super(message);
-    }
-}
