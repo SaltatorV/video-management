8163881eb43fd2302317667c07d278b70bf54e29
Saltator <saltatorv@gmail.com>
2024-01-05 17:56:52 +0100

    feat: update bean config with new functionality #6

--- video-management-container/src/main/java/com/video/management/container/BeanConfiguration.java
+++ video-management-container/src/main/java/com/video/management/container/BeanConfiguration.java
@@ -1,28 +1,39 @@
 package com.video.management.container;
 
+import com.video.management.service.dto.UserSnapshot;
 import com.video.management.service.dto.command.AddToFavoriteCommand;
 import com.video.management.service.dto.response.MessageResponse;
 import com.video.management.service.dto.response.VideoDataResponse;
 import com.video.management.service.port.input.UserCommandFacade;
 import com.video.management.service.port.input.UserQueryFacade;
 import com.video.management.service.port.input.VideoQueryFacade;
+import com.video.management.service.port.output.UserCommandRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class BeanConfiguration {
 
     @Bean
-    UserCommandFacade userCommandFacade() {
-        return new UserCommandFacade() {
+    UserCommandRepository userCommandRepository() {
+        Set<UserSnapshot> snapshots = new HashSet<>();
+        return new UserCommandRepository() {
             @Override
-            public MessageResponse addVideoToUserFavorites(String username, AddToFavoriteCommand command) {
-                return MessageResponse.create("Success");
+            public void addToFavorite(UserSnapshot snapshot) {
+                snapshots.add(snapshot);
+            }
+
+            @Override
+            public boolean existsBySnapshot(UserSnapshot snapshot) {
+                return snapshots.contains(snapshot);
             }
         };
     }
+
 
     @Bean
     UserQueryFacade userQueryFacade() {
