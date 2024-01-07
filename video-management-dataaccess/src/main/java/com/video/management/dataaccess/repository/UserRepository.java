cb9e83d0a22f08a8d856bb840a41a38a22a5e0de
Saltator <saltatorv@gmail.com>
2024-01-07 14:45:01 +0100

    feat: update jpa repository with findByUsername #10

--- video-management-dataaccess/src/main/java/com/video/management/dataaccess/repository/UserRepository.java
+++ video-management-dataaccess/src/main/java/com/video/management/dataaccess/repository/UserRepository.java
@@ -2,7 +2,13 @@ package com.video.management.dataaccess.repository;
 
 import com.video.management.dataaccess.entity.UserFavoriteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserFavoriteEntity, Long> {
     boolean existsByUsernameAndVideoTitle(String username, String videoTitle);
+
+    @Query("SELECT u.videoTitle from UserFavoriteEntity u WHERE u.username = :username")
+    List<String> findByUsername(String username);
 }
