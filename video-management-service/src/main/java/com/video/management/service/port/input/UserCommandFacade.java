366b2dfd62fe9574dfda5c354a6d09fd4681dd29
Saltator <saltatorv@gmail.com>
2024-01-07 19:16:18 +0100

    feat: configure UserCommandFacade to validate command #11

--- video-management-service/src/main/java/com/video/management/service/port/input/UserCommandFacade.java
+++ video-management-service/src/main/java/com/video/management/service/port/input/UserCommandFacade.java
@@ -2,7 +2,8 @@ package com.video.management.service.port.input;
 
 import com.video.management.service.dto.command.AddToFavoriteCommand;
 import com.video.management.service.dto.response.MessageResponse;
+import jakarta.validation.Valid;
 
 public interface UserCommandFacade {
-    MessageResponse addVideoToUserFavorites(String username, AddToFavoriteCommand command);
+    MessageResponse addVideoToUserFavorites(String username, @Valid AddToFavoriteCommand command);
 }
