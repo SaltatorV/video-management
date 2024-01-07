64e68cf597f137e7b577afeb4298aa6e82bf4dff
Saltator <saltatorv@gmail.com>
2024-01-07 14:52:57 +0100

    feat: add h2 database #10

--- video-management-dataaccess/src/main/java/com/video/management/dataaccess/repository/UserRepository.java
+++ video-management-dataaccess/src/main/java/com/video/management/dataaccess/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.video.management.dataaccess.repository;
 import com.video.management.dataaccess.entity.UserFavoriteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserFavoriteEntity, Long> {
     boolean existsByUsernameAndVideoTitle(String username, String videoTitle);
 
